345d843a3b546e8bbd847abe881a97b51b603889
mahbub-hr <mahbub.mmhr@gmail.com>
2020-04-06 21:01:54 +0600

    added blockchain class

--- peer.py
+++ peer.py
@@ -1,3 +1,6 @@
+from hashlib import sha256
+import json
+import time
 import socket
 import selectors
 
@@ -14,6 +17,93 @@ GLOBAL_BLOCK_COUNT = 0
 
 class worldState:
     pass
+
+
+class Block:
+    def __init__(self, index, transactions, timestamp, previous_hash):
+        """
+        Constructor for the `Block` class.
+        :param index:         Unique ID of the block.
+        :param transactions:  List of transactions.
+        :param timestamp:     Time of generation of the block.
+        :param previous_hash: Hash of the previous block in the chain which this block is part of.                                        
+        """
+        self.index = index
+        self.transactions = transactions
+        self.timestamp = timestamp
+        self.previous_hash = previous_hash  # Adding the previous hash field
+
+
+    def compute_hash(self):
+        """
+        Returns the hash of the block instance by first converting it
+        into JSON string.
+        """
+        block_string = json.dumps(self.__dict__,
+                                  sort_keys=True)  # The string equivalent also considers the previous_hash field now
+        return sha256(block_string.encode()).hexdigest()
+
+
+class Blockchain:
+
+    def __init__(self):
+        """
+        Constructor for the `Blockchain` class.
+        """
+        self.chain = []
+        self.create_genesis_block()
+
+    def create_genesis_block(self):
+        """
+        A function to generate genesis block and appends it to
+        the chain. The block has index 0, previous_hash as 0, and
+        a valid hash.
+        """
+        genesis_block = Block(0, [], time.time(), "0")
+        genesis_block.hash = genesis_block.compute_hash()
+        self.chain.append(genesis_block)
+
+    @property
+    def last_block(self):
+        """
+        A quick pythonic way to retrieve the most recent block in the chain. Note that
+        the chain will always consist of at least one block (i.e., genesis block)
+        """
+        return self.chain[-1]
+
+    def add_block(self, block):
+        """
+        A function that adds the block to the chain after verification.
+        Verification includes:******validation not implemented yet********
+        * Checking if the proof is valid.
+        * The previous_hash referred in the block and the hash of a latest block
+          in the chain match.
+        """
+        previous_hash = self.last_block.hash
+
+        if previous_hash != block.previous_hash:
+            return False
+
+        self.chain.append(block)
+        return True
+
+    def is_valid(self):
+        """
+        need to be implemented
+        :return:
+        """
+    pass
+
+    def persist_chain(self):
+        """
+        save blockchain to disk
+        :return:
+        """
+
+        return
+
+
+blockchain = Blockchain()
 
 
 def block_write(block):
@@ -34,6 +124,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
 
     sock.listen(5)
     print('listenning at : ',TCP_IP, 'Port: ', T_PORT)
+    blockstr = ""
     while True:
         conn, addr = sock.accept()
         with conn:
@@ -46,9 +137,10 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
                     break
                 print("s: ", data)
 
-        block = ""
-        block = block + str.decode()+'\n'
+        blockstr += str.decode()+'\n'
         COUNT += 1
         if COUNT == BLOCK_SIZE:
-            block_write(block)
+            block = Block(GLOBAL_BLOCK_COUNT, blockstr, time.time(), blockchain.last_block.hash)
+            blockchain.add_block(block)
             COUNT = 0
+            block = ""
