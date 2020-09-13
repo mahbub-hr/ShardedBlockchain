744d81fbe96919c8bd81641f57b9fd2f756ef416
mahubb-hr <mahubb.mmhr@gmail.com>
2020-09-14 01:03:25 +0600

    hello

--- peer/peer.py
+++ peer/peer.py
@@ -734,9 +734,9 @@ if __name__ == '__main__':
     NODE_NUMBER = port
     # NODE_NUMBER = args.node
     IS_ANCHOR = args.anchor
-    host_ip =  get_ext_ip()
+    host_ip =  get_host_ip()
     
-    SELF_KEY = "http://" + host_ip + ":" + repr(port)+"/"
+    SELF_KEY = "http://" + get_ext_ip() + ":" + repr(port)+"/"
     print(SELF_KEY)
     peer_insert(get_my_key())
     app.run(host=host_ip, port=port, debug=True)
